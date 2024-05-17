d35bc5c1b1d329696b8a702e4cc56a32ea0bcc99
Kevin Liu <javaandroidc@qq.com>
2024-05-17 23:04:01 +0800

    update file by keivn

--- pushfile_copy.cmd
+++ pushfile_copy.cmd
@@ -4,3 +4,4 @@ cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\Langchain-Chatbot-Wiki
 xcopy /E /Y * "D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\6LLM.github.io" 
 cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\6LLM.github.io
 git add . && git commit -m"update file by keivn" && git push
+exit
