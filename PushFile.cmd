d1e8c20b6e7dd965bc1610fd312748964034238c
Kevin Liu <javaandroidc@qq.com>
2024-05-17 23:01:38 +0800

    update file by keivn

--- PushFile.cmd
+++ PushFile.cmd
@@ -1,11 +1,7 @@
 
 d:
 cd D:\sourcecode\ecap-NN\ChatGPT\sourcecode\2024\myGithub\Langchain-Chatbot-Wiki\
-
-
-gitbook build
-
-start pushfile_copy.cmd
+gitbook build && start pushfile_copy.cmd
 
 
 
